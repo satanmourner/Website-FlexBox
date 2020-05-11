5f5220850c380844442c1e97899fe5701e075016
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-05-11 20:50:27 +0430

    first commit

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 $(".logo").click( () => {
   $target = $('html,body');
-  $target.animate({scrollTop: $("section").offset().top}, 1500,);
+  $target.animate({scrollTop: $("section").offset().top}, 1500);
 });
 
 document.getElementById("date").innerHTML = new Date().toDateString();
 
-
-
+var img = document.querySelectorAll("img");
+var cover = document.getElementsByClassName("cover-img");
+$(document).ready(function() {
+  $(".card-1").mouseenter( function () {
+    // console.log(this);
+    for(var i = 0; i < img.length, i < cover.length; i++) {
+      cover[i].style.backgroundImage = 'url('+img[i].src+')';
+    }
+  });
+});
