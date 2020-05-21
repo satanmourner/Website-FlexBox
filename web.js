fd0b2db88118e887187fa0f10629e8faccdbc9b2
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-05-22 00:07:47 +0430

    second commit

--- web.js
+++ web.js
@@ -1,17 +1,24 @@
-$(".logo").click( () => {
+$(".logo").mousedown(() => {
   $target = $('html,body');
-  $target.animate({scrollTop: $("section").offset().top}, 1500);
+  $target.animate({ scrollTop: $("section").offset().top }, 1500);
+});
+
+$("#article").click(() => {
+  $target = $('html,body');
+  $target.animate({ scrollTop: $("article").offset().top }, 1000);
 });
 
 document.getElementById("date").innerHTML = new Date().toDateString();
 
-var img = document.querySelectorAll("img");
-var cover = document.getElementsByClassName("cover-img");
-$(document).ready(function() {
-  $(".card-1").mouseenter( function () {
-    // console.log(this);
-    for(var i = 0; i < img.length, i < cover.length; i++) {
-      cover[i].style.backgroundImage = 'url('+img[i].src+')';
-    }
-  });
+$("#sub").click( () => {
+  var str = $("input").val();
+  if(str.match(/\w+@[a-z]+\.([a-z]{2,4})$/g) || str.match(/\w+@[a-z]+\.([a-z]{2,4})+\.([a-z]{2,4})$/g)) {
+    alert("correct");
+}
 });
+
+var img = document.getElementsByClassName("img-card");
+var cover = document.getElementsByClassName("cover-img");
+for (var i = 0; i < img.length; i++) {
+  cover[i].style.backgroundImage = 'url(' + img[i].src + ')';
+}
