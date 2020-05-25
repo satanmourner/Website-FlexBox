ea84e0379eac5d649fdfc61eec53eb2e9ad736b1
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-05-26 03:04:02 +0430

    last commit

--- web.js
+++ web.js
@@ -1,24 +1,41 @@
-$(".logo").mousedown(() => {
+$(document).ready(() => {
+
   $target = $('html,body');
-  $target.animate({ scrollTop: $("section").offset().top }, 1500);
+  $(".logo").mousedown(() => $target.animate({ scrollTop: $("section").offset().top }, 1500));
+
+  $(".article").click(() => $target.animate({ scrollTop: $("article").offset().top }, 1000));
+
+  $(".cards").click(() => $target.animate({ scrollTop: $(".grid-cards").offset().top }, 1000));
+
+  $(".footer").click(() => $target.animate({ scrollTop: $("footer").offset().top }, 1000));
+
+  $(document).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $("#goUp").css("display", "block");
+    } else {
+      $("#goUp").css("display", "none");
+    }
+  });
+
+  $("#goUp").click(() => $target.animate({ scrollTop: $("body").offset().top }, 1000));
+
+  document.getElementById("date").innerHTML = new Date().toDateString();
+
+  $("#sub").click(function (e) {
+    var str = $("input").val();
+    if (str.match(/\w+@[a-z]+\.([a-z]{2,4})$/g) || str.match(/\w+@[a-z]+\.([a-z]{2,4})+\.([a-z]{2,4})$/g)) {
+      $("#onCover").text("email sended").css("color", "pink");
+    }
+    e.preventDefault();
+    $("input").val("");
+  });
+
+  var img = document.getElementsByClassName("img-card");
+  var cover = document.getElementsByClassName("cover-img");
+  for (var i = 0; i < img.length; i++) {
+    cover[i].style.backgroundImage = 'url(' + img[i].src + ')';
+  }
+
+  $("img").attr("draggable", "false");
+
 });
-
-$("#article").click(() => {
-  $target = $('html,body');
-  $target.animate({ scrollTop: $("article").offset().top }, 1000);
-});
-
-document.getElementById("date").innerHTML = new Date().toDateString();
-
-$("#sub").click( () => {
-  var str = $("input").val();
-  if(str.match(/\w+@[a-z]+\.([a-z]{2,4})$/g) || str.match(/\w+@[a-z]+\.([a-z]{2,4})+\.([a-z]{2,4})$/g)) {
-    alert("correct");
-}
-});
-
-var img = document.getElementsByClassName("img-card");
-var cover = document.getElementsByClassName("cover-img");
-for (var i = 0; i < img.length; i++) {
-  cover[i].style.backgroundImage = 'url(' + img[i].src + ')';
-}
